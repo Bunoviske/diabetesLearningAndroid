d2c5a9a929a9e00989dfccaf05aa097d35ea29f3
bunoviske <brunofcarvalho1996@gmail.com>
2018-10-24 11:47:32 -0200

    Implementação do Canvas sobre uma imagem bitmap

--- app/src/main/java/com/example/bruno/diabeteslearning/MainActivity.java
+++ app/src/main/java/com/example/bruno/diabeteslearning/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.bruno.diabeteslearning;
 
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,40 +15,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import android.util.Log;
 import android.view.SurfaceView;
+import android.util.DisplayMetrics;
 import android.view.WindowManager;
-
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCameraView;
-import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "MainActivity";
-    private boolean isPictureTaken = false;
-    Mat plate;
-    JavaCameraView javaCameraView;
-    BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status){
-                case BaseLoaderCallback.SUCCESS:{
-                    javaCameraView.enableView();
-                    break;
-                }
-                default:{
-                    super.onManagerConnected(status);
-                    break;
-                }
-            }
-        }
-    };
+    private ImageViewCanvas imageViewCanvas;
 
     static {
         if (OpenCVLoader.initDebug()){
@@ -58,21 +41,28 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        FloatingActionButton takePic = findViewById(R.id.takePicButton);
-        takePic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isPictureTaken = true;
-            }
-        });
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-        javaCameraView = findViewById(R.id.java_camera_view);
-        javaCameraView.setVisibility(SurfaceView.VISIBLE);
-        javaCameraView.setCvCameraViewListener(this);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.oi);
+
+
+        imageViewCanvas = findViewById(R.id.paintView);
+
+        //int height = metrics.heightPixels;
+        int width = metrics.widthPixels;
+
+        //altura dp declarada no arquivo xml. N consegui extrair dinamicamente
+        int heightDp = 396;
+        int height = Math.round(heightDp * (metrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+
+        bitmap = Bitmap.createScaledBitmap(bitmap, width, height, true);
+
+        imageViewCanvas.init(bitmap);
+
+
 
     }
     @Override
@@ -80,72 +70,10 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         super.onResume();
         if (OpenCVLoader.initDebug()){
             Log.i(TAG, "Opencv OK Resume");
-            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
         else{
             Log.i(TAG, "Opencv Error");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0,this,mLoaderCallback);
         }
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        if (javaCameraView != null){
-            javaCameraView.disableView();
-        }
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        if (javaCameraView != null){
-            javaCameraView.disableView();
-        }
-    }
-
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-        //plate = new Mat(height,width, CvType.CV_8UC3);
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-
-        //plate.release();
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        if (isPictureTaken){
-            onCameraViewStopped();
-        }
-        else {
-            plate = inputFrame.rgba();
-        }
-        return plate;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
