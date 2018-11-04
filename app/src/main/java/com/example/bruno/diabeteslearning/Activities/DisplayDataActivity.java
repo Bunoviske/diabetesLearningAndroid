3d32eec892d063a3e4f3ef559b00504526a4d9b6
bunoviske <brunofcarvalho1996@gmail.com>
2018-11-03 23:56:22 -0200

    Implementacao da activity que pede peso total dos alimentos e informa dados de CHO

---
+++ app/src/main/java/com/example/bruno/diabeteslearning/Activities/DisplayDataActivity.java
@@ -0,0 +1,25 @@
+package com.example.bruno.diabeteslearning.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.widget.TextView;
+
+import com.example.bruno.diabeteslearning.R;
+
+public class DisplayDataActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_carbohydrate_data);
+
+
+    }
+
+}
--- app/src/main/java/com/example/bruno/diabeteslearning/MainActivity.java
+++ app/src/main/java/com/example/bruno/diabeteslearning/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.bruno.diabeteslearning;
 
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.bruno.diabeteslearning.Activities.DisplayDataActivity;
 import com.example.bruno.diabeteslearning.ImagePaint.ImageViewCanvas;
 
 import org.opencv.android.OpenCVLoader;
@@ -31,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setButton();
 
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
@@ -65,6 +71,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void setButton(){
+        Button button = findViewById(R.id.nextPageButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent activity = new Intent(MainActivity.super.getBaseContext(),
+                        DisplayDataActivity.class);
+                startActivity(activity);
+            }
+        });
+    }
 
     //public native String stringFromJNI();
 }
