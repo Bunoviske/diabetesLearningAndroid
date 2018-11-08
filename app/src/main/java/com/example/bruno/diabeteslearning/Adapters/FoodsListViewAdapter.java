345adc53f9a320d2491d90d702ea7441d8ac88b7
bunoviske <brunofcarvalho1996@gmail.com>
2018-11-07 23:31:00 -0200

    Implementação do listView dos alimentos marcados na foto

---
+++ app/src/main/java/com/example/bruno/diabeteslearning/Adapters/FoodsListViewAdapter.java
@@ -0,0 +1,83 @@
+package com.example.bruno.diabeteslearning.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.bruno.diabeteslearning.ImagePaint.ImageViewCanvas;
+import com.example.bruno.diabeteslearning.R;
+
+import java.util.ArrayList;
+
+public class FoodsListViewAdapter extends  RecyclerView.Adapter<FoodsListViewAdapter.MyViewHolder>{
+
+    private ArrayList<String> mDataset;
+    private ImageViewCanvas imageViewCanvas;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ImageButton mButton;
+
+        public MyViewHolder(View v) {
+            super(v);
+            mTextView = v.findViewById(R.id.foodNameTile);
+            mButton = v.findViewById(R.id.buttonTile);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public FoodsListViewAdapter(ArrayList<String> dataset, ImageViewCanvas imgCanvas) {
+        imageViewCanvas = imgCanvas;
+        mDataset = dataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public FoodsListViewAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        View view = LayoutInflater.from(
+                parent.getContext()).inflate(R.layout.item_foods_list_view, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(view);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset.get(position));
+
+        holder.mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mDataset.remove(position);
+                notifyDataSetChanged();
+                imageViewCanvas.removeSpecificAreaFromPath(position);
+                imageViewCanvas.callClearSpecificPath(position);
+
+            }
+        });
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+
+}
