ca379f941442879acfe72a01e538d36082e66017
bunoviske <brunofcarvalho1996@gmail.com>
2018-11-08 18:02:16 -0200

    Implementação de classe com todas propriedades de uma refeição e atualizacao da logica entre as classes do package Carbohydrate

--- app/src/main/java/com/example/bruno/diabeteslearning/Carbohydrate/CarboDetector.java
+++ app/src/main/java/com/example/bruno/diabeteslearning/Carbohydrate/CarboDetector.java
@@ -4,13 +4,8 @@ import android.util.Log;
 import java.util.ArrayList;
 
 
-public class CarboDetector {
+public class CarboDetector extends MealProperties{
 
-    //TODO - variaveis a serem ENVIADAS para o firebase
-    private float totalFoodWeigh;
-    private float totalCarbo;
-
-    private ArrayList<FoodRegion> foods = new ArrayList<>();
     private float constant;
 
     private static String TAG = "CarboDetector";
@@ -23,11 +18,36 @@ public class CarboDetector {
         }
     }
 
-    public void setTotalFoodWeigh(float totalFoodWeigh) {
-        this.totalFoodWeigh = totalFoodWeigh;
+    public float getTotalFoodWeight(){
+        return totalFoodWeight;
     }
 
-    public void saveFoodRegion(int regionPixeis, String foodName){
+    public float getInsulinDose(){
+        return insulinDose;
+    }
+
+    public float getInsulinCarboRelation(){
+        return insulinCarboRelation;
+    }
+
+    public float getTotalCarbo(){
+        return totalCarbo;
+    }
+
+    public ArrayList<FoodRegion> getFoods(){
+        return foods;
+    }
+
+
+    public void setTotalFoodWeight(float totalFoodWeight) {
+        super.totalFoodWeight = totalFoodWeight;
+    }
+
+    public void setInsulinCarboRelation(float insulinCarboRelation){
+        super.insulinCarboRelation = insulinCarboRelation;
+    }
+
+    private void saveFoodRegion(int regionPixeis, String foodName){
 
         float carboRelation = 0, foodDensity = 0;
         //TODO - CHAMAR FUNCAO QUE PEGA RELACAO DE CHO E DENSIDADE DO ALIMENTO NO FIREBASE
@@ -42,19 +62,23 @@ public class CarboDetector {
         for(int i = 0; i < foods.size();i++){
             aux += (foods.get(i).foodDensity * foods.get(i).regionPixeis);
         }
-        constant = totalFoodWeigh/aux;
+        constant = totalFoodWeight/aux;
 
         for(int i = 0; i < foods.size();i++){
-            foods.get(i).weigh = foods.get(i).foodDensity * foods.get(i).regionPixeis * constant;
-            foods.get(i).carbo = foods.get(i).weigh * foods.get(i).carboRelation;
+            foods.get(i).weight = foods.get(i).foodDensity * foods.get(i).regionPixeis * constant;
+            foods.get(i).carbo = foods.get(i).weight * foods.get(i).carboRelation;
             totalCarbo += foods.get(i).carbo;
 
             Log.i(TAG, foods.get(i).foodName + "  Peso: "
-                    + foods.get(i).weigh + " Carbo: " + foods.get(i).carbo);
+                    + foods.get(i).weight + " Carbo: " + foods.get(i).carbo);
 
         }
 
+        if (insulinCarboRelation > 0)
+            insulinDose = totalCarbo/insulinCarboRelation;
+
         Log.i(TAG, "Total de carboidrato: " + totalCarbo);
+        Log.i(TAG, "Dose de insulina: " + insulinDose);
     }
 
 }
